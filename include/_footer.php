387ce77602f726dac203b33e27287a1f065d26ef
toriuri <laudate0419@gmail.com>
2021-06-19 02:45:25 +0900

    상품소개 페이지 UI 개발 --> php 반영

--- include/_footer.php
+++ include/_footer.php
@@ -1,56 +1,74 @@
-				<footer class="footer">
-					<div class="footer-wrap">
+				
+			</main>
+			
+			<footer class="footer">
+				<div class="footer-wrap">
+					
+					<div class="box-left">
+						<ul class="terms-list">
+							<li><a href="../policy/privacy.php"><strong>개인정보처리방침</strong></a></li>
+							<li><a href="../policy/service.php">이용약관</a></li>
+							<li><a href="http://www.kti.co.kr/" target="_blank">넷케이티아이 <i class="xi-external-link"></i></a></li>
+						</ul>
 						
-						<div class="box-left">
-							<ul class="terms-list">
-								<li><a href="../policy/privacy.php"><strong>개인정보처리방침</strong></a></li>
-								<li><a href="../policy/service.php">이용약관</a></li>
-								<li><a href="http://www.kti.co.kr/" target="_blank">넷케이티아이 <i class="xi-external-link"></i></a></li>
-							</ul>
+						<dl class="contact-list">
+							<dt>E-mail.</dt>
+							<dd><a href="mailto:teamsvoice@kti.co.kr">teamsvoice@kti.co.kr</a></dd>
+							<dt>Tel.</dt>
+							<dd><a href="tel:031-786-9990">031-786-9990</a></dd>
+						</dl>
+					</div>
+					
+					<div class="box-right">
+						<div class="family-site-box">
 							
-							<dl class="contact-list">
-								<dt>E-mail.</dt>
-								<dd><a href="mailto:teamsvoice@kti.co.kr">teamsvoice@kti.co.kr</a></dd>
-								<dt>Tel.</dt>
-								<dd><a href="tel:031-786-9990">031-786-9990</a></dd>
-							</dl>
-						</div>
-						
-						<div class="box-right">
-							<div class="family-site-box">
-								
-								<div class="list-box">
-									<ul>
-										<li>
-											<a href="https://www.naver.com/">네이버</a>
-										</li>
-										<li>
-											<a href="https://www.daum.net/">다음</a>
-										</li>
-										<li>
-											<a href="https://www.google.com/">구글</a>
-										</li>
-									</ul>
-								</div>
-								
-								<a href="#list-box" class="btn-family-site">유관기관 바로가기 <i class="xi-angle-down-min"></i></a>
-							
+							<div class="list-box">
+								<ul>
+									<li>
+										<a href="https://www.naver.com/">네이버</a>
+									</li>
+									<li>
+										<a href="https://www.daum.net/">다음</a>
+									</li>
+									<li>
+										<a href="https://www.google.com/">구글</a>
+									</li>
+								</ul>
 							</div>
 							
-							<p class="copyright">Copyright &copy; netKTI Co., Ltd. All Rights Reserved</p>
+							<a href="#list-box" class="btn-family-site">유관기관 바로가기 <i class="xi-angle-down-min"></i></a>
+						
 						</div>
-					
+						
+						<p class="copyright">Copyright &copy; netKTI Co., Ltd. All Rights Reserved</p>
 					</div>
-				</footer>
+				
+				</div>
+			</footer>
 			
-			</main>
-		
 		</div>
-		
+	
 		<script src="../assets/js/gsap/gsap.min.js"></script>
 		<script src="../assets/js/gsap/ScrollTrigger.min.js"></script>
 		<script src="../assets/js/script.js"></script>
 		<script src="../assets/js/page/main.js"></script>
-	
+			
+			<!-- Swiper JS	-->
+			<script src="../assets/js/swiper-5.3.0/swiper.js"></script>
+			
+			<script>
+				//swiper
+				var swiper = new Swiper(".product-container", {
+					slidesPerView: "auto",
+					//spaceBetween: 40,
+					loop: true,
+					autoplay: true,
+					pagination: {
+						el: ".swiper-pagination",
+						clickable: true,
+					},
+				});
+			</script>
+			
 	</body>
 </html>
