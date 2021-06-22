da84630fa5a9c00e11063eaea7a099aa13c731cf
toriuri <laudate0419@gmail.com>
2021-06-22 18:49:16 +0900

    1차 QA 적용 중 / 불필요한 스크립트 및 플러그인 삭제

--- include/_footer.php
+++ include/_footer.php
@@ -1,57 +1,54 @@
 				
-			</main>
-			
-			<footer class="footer">
-				<div class="footer-wrap">
-					
-					<div class="box-left">
-						<ul class="terms-list">
-							<li><a href="../policy/privacy.php"><strong>개인정보처리방침</strong></a></li>
-							<li><a href="../policy/service.php">이용약관</a></li>
-							<li><a href="http://www.kti.co.kr/" target="_blank">넷케이티아이 <i class="xi-external-link"></i></a></li>
-						</ul>
+				</main>
+				
+				<footer class="footer">
+					<div class="footer-wrap">
 						
-						<dl class="contact-list">
-							<dt>E-mail.</dt>
-							<dd><a href="mailto:teamsvoice@kti.co.kr">teamsvoice@kti.co.kr</a></dd>
-							<dt>Tel.</dt>
-							<dd><a href="tel:031-786-9990">031-786-9990</a></dd>
-						</dl>
-					</div>
-					
-					<div class="box-right">
-						<div class="family-site-box">
+						<div class="box-left">
+							<ul class="terms-list">
+								<li><a href="../policy/privacy.php"><strong>개인정보처리방침</strong></a></li>
+								<li><a href="../policy/service.php">이용약관</a></li>
+								<li><a href="http://www.kti.co.kr/" target="_blank">넷케이티아이 <i class="xi-external-link"></i></a></li>
+							</ul>
 							
-							<div class="list-box">
-								<ul>
-									<li>
-										<a href="https://www.naver.com/">네이버</a>
-									</li>
-									<li>
-										<a href="https://www.daum.net/">다음</a>
-									</li>
-									<li>
-										<a href="https://www.google.com/">구글</a>
-									</li>
-								</ul>
-							</div>
-							
-							<a href="#list-box" class="btn-family-site">유관기관 바로가기 <i class="xi-angle-down-min"></i></a>
-						
+							<dl class="contact-list">
+								<dt>E-mail.</dt>
+								<dd><a href="mailto:teamsvoice@kti.co.kr">teamsvoice@kti.co.kr</a></dd>
+								<dt>Tel.</dt>
+								<dd><a href="tel:031-786-9990">031-786-9990</a></dd>
+							</dl>
 						</div>
 						
-						<p class="copyright">Copyright &copy; netKTI Co., Ltd. All Rights Reserved</p>
+						<div class="box-right">
+							<div class="family-site-box">
+								
+								<div class="list-box">
+									<ul>
+										<li>
+											<a href="https://www.naver.com/">네이버</a>
+										</li>
+										<li>
+											<a href="https://www.daum.net/">다음</a>
+										</li>
+										<li>
+											<a href="https://www.google.com/">구글</a>
+										</li>
+									</ul>
+								</div>
+								
+								<a href="#list-box" class="btn-family-site">유관기관 바로가기 <i class="xi-angle-down-min"></i></a>
+							
+							</div>
+							
+							<p class="copyright">Copyright &copy; netKTI Co., Ltd. All Rights Reserved</p>
+						</div>
+					
 					</div>
+				</footer>
 				
-				</div>
-			</footer>
-			
-		</div>
-	
-		<script src="../assets/js/gsap/gsap.min.js"></script>
-		<script src="../assets/js/gsap/ScrollTrigger.min.js"></script>
-		<script src="../assets/js/script.js"></script>
-		<script src="../assets/js/page/main.js"></script>
+			</div>
+		
+			<script src="../assets/js/script.js"></script>
 			
 			<!-- Swiper JS	-->
 			<script src="../assets/js/swiper-5.3.0/swiper.js"></script>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 		<meta name="format-detection" content="telephone=yes">
 		
 		<title>팀즈 클라우드 보이스 커넥트</title>
@@ -612,10 +612,7 @@
 		
 		</div>
 		
-		<script src="./assets/js/gsap/gsap.min.js"></script>
-		<script src="./assets/js/gsap/ScrollTrigger.min.js"></script>
 		<script src="./assets/js/script.js"></script>
-		<script src="./assets/js/page/main.js"></script>
 		
 		<!-- fullpage JS -->
 		<script src="./assets/js/fullpage/dist/fullpage.js"></script>
